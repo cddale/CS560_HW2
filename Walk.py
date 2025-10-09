315a268668c72ff499149097bfadd8f9762cb8a5
Cloie Dale <cddale@crimson.ua.edu>
2025-10-09 18:06:49 -0500

    acts as wall follower (not  at ideal proformance)

--- Walk.py
+++ Walk.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from datetime import datetime
 import math
-import np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
@@ -10,10 +9,10 @@ class Walk(Node):
     def __init__(self):
         super().__init__('walk')
         #PID values
-        self.g = 2.5
-        self.K_P = 5
-        self.K_I = 4
-        self.K_D = 2
+        self.g = 1.5
+        self.K_P = 100
+        self.K_I = 0
+        self.K_D = 0
         self.start = datetime.now()
 
         self.cmd_pub = self.create_publisher(Twist,'/cmd_vel', 10)
@@ -32,45 +31,51 @@ class Walk(Node):
 
     def sensor_callback(self, msg): #make this more robust for repersentation
         middle_sensor = int(len(msg.ranges) / 2)
-        #left_sensor = int(len(msg.ranges) / 4)
-        #right_sensor = int(len(msg.ranges) / 4) * 3
+        left_sensor = int(len(msg.ranges) / 5)
+        right_sensor = int(len(msg.ranges) / 5) * 4
 
-        #left = msg.ranges[left_sensor]
-        #right = msg.ranges[right_sensor]
-        middle_sector = msg.ranges[middle_sensor - 25 : middle_sensor + 25]
-        weights = np.hanning(len(middle_sector))  # bell curve for weights
-        self.whisker = np.average(middle_sector, weights=weights)
+        middle = msg.ranges[middle_sensor]
+        middle_sector = msg.ranges[left_sensor : right_sensor]
+        min_point = min(middle_sector)
         
         print("Sensor: " + str(self.whisker) + "; U(t): " + str(self.move_cmd.angular.z))
-        #self.whisker = (right + left)/2
+        
+        if min_point < 1:
+            self.whisker = ((sum(middle_sector) / (right_sensor-left_sensor)) * .25) + min_point * .75
+        else:
+            self.whisker = ((sum(middle_sector) / (right_sensor-left_sensor)) * .80) + middle * 0.2
+
 		
     def forward(self):
 	    self.move_cmd.linear.x = self.linear_speed 
 
     def timer_callback(self): #change this to use the PID formula (uses information from sensor_callback)
+        normal_range = 2500
         self.e = self.g - self.whisker
 
-        timeDiff = datetime.now() - self.start
+        timeDiff = (datetime.now() - self.start).total_seconds()
 
-        self.e_sum = self.e_sum + self.e  * ((self.e - self.e_prev) / max(timeDiff.total_seconds(),0.01))
+        self.e_sum = self.e_sum + self.e  * ((self.e - self.e_prev) / max(timeDiff,0.01))
 
-        dedt = (self.e - self.e_prev) / (max(self.e - self.e_prev, 0.01) / max(timeDiff.total_seconds(), 0.01))
+        dedt = (self.e - self.e_prev) / (max(self.e - self.e_prev, 0.01) / max(timeDiff, 0.01))
 
         u = self.K_P * self.e + self.K_I * self.e_sum + self.K_D * dedt
-        self.move_cmd.angular.z = ((u + 3000) / (3000 + 3000)) * (5 + 5) + -5
+        
+        self.move_cmd.angular.z = u
+
+        if self.whisker < 1.25:
+            self.move_cmd.linear.x = 0.2
+            """ if timeDiff % 5:
+                self.move_cmd.angular.z = -self.move_cmd.angular.z """
+        else:
+            self.move_cmd.linear.x = 0.8
+        #self.move_cmd.angular.z = ((u + normal_range) / (normal_range + normal_range)) * (5 + 5) + -5
         
         self.e_prev = self.e
 
         self.cmd_pub.publish(self.move_cmd) 
 
-        """ if(self.whisker < 2.0):
-            self.move_cmd.angular.z = 2.0
-            self.move_cmd.linear.x = 0.2
-        else:
-            self.move_cmd.angular.z = 0.0
-            self.move_cmd.linear.x = 0.8
-        self.cmd_pub.publish(self.move_cmd) 
-		 """
+        
 def main(args=None):
     rclpy.init()
     walk_node = Walk()
